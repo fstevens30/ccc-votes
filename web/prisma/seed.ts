a4d29fddfa3d0b32fc96940b1b5dc69465905993
Flynn Stevens <flynnstevens30@gmail.com>
2025-09-06 07:24:26 +1200

    Fixed seed scripts for councillor data

--- web/prisma/seed.ts
+++ web/prisma/seed.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import 'dotenv/config'
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DIRECT_URL
-    }
-  }
-})
+const prisma = new PrismaClient()
 
 async function main () {
   console.log('Seed script started!')
